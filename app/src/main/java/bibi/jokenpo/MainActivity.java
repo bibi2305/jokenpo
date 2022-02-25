7cb1ebd057419cda1a06066d6fae9293f697284b
BIANCA RIBEIRO BARROS <bianca.rbarros1@senacsp.edu.br>
2022-02-25 10:19:54 -0300

    Altera o jogo para gerar resultads aleatórios para mão 2 ao clicar no  texto jogar

--- app/src/main/java/bibi/jokenpo/MainActivity.java
+++ app/src/main/java/bibi/jokenpo/MainActivity.java
@@ -3,39 +3,64 @@ package bibi.jokenpo;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.TextView;
+
 
 public class MainActivity extends Activity {
     // variaveis
     private ImageView player1;
     private ImageView player2;
+    private int joga1 = 0;
+    private TextView btJogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //elementos na tela
         player1 = findViewById(R.id.p1);
         player2 = findViewById(R.id.p2);
+        btJogar = findViewById(R.id.txtplay);
 
+        // Evento ao clicar para alterar a mão 1
         player1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast alerta = Toast.makeText(getApplicationContext(),"player 1 selecionado", Toast.LENGTH_LONG);
-                alerta.show();
-                Log.i("aviso","oi player 1");
+                if(joga1 == 0){
+                    player1.setImageResource(R.drawable.papel);
+                    joga1++;
+                }else if(joga1 == 1){
+                    player1.setImageResource(R.drawable.tesoura);
+                    joga1++;
+                }else if(joga1 == 2){
+                    player1.setImageResource(R.drawable.pedra);
+                    joga1 = 0;
+                }
             }
         });
 
-        player2.setOnClickListener(new View.OnClickListener() {
+        //Evento ao clicar em jogar
+        btJogar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast alerta2 = Toast.makeText(getApplicationContext(),"player 2 selecionado", Toast.LENGTH_LONG);
-                alerta2.show();
-                Log.i("aviso","oi player 2");
+                geraPlayer2();
             }
         });
+    }
+
+    // Evento de alterar de forma aleatória a mão 2.
+    private void geraPlayer2(){
+        int joga2 = (int)(Math.random()*150);
+        if(joga2 >= 0 && joga2 < 50){
+            player2.setImageResource(R.drawable.papel);
+        }else if(joga2 >= 50 && joga2 < 100){
+            player2.setImageResource(R.drawable.tesoura);
+        }else if(joga2 >= 100){
+            player2.setImageResource(R.drawable.pedra);
+        }
+        joga2 = 0;
     }
 }
